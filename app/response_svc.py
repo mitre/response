0a380c98695392e8bd1cf03fc87857ce365ab74a
Hunt, David <davidhunt@mitre.org>
2020-02-15 15:10:25 -0500

    removing dead code

--- app/response_svc.py
+++ app/response_svc.py
@@ -7,8 +7,6 @@ class ResponseService(BaseService):
 
     def __init__(self, services):
         self.log = self.add_service('response_svc', self)
-        self.file_svc = services.get('file_svc')
-        self.app_svc = services.get('app_svc')
         self.data_svc = services.get('data_svc')
 
     @template('response.html')
