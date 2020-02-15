4220154e11483b0d422a615ce7d1b96ce2a1302a
Hunt, David <davidhunt@mitre.org>
2020-02-15 16:42:46 -0500

    making blue parsers work a bit different than red ones

--- app/parsers/process.py
+++ app/parsers/process.py
@@ -7,14 +7,13 @@ class Parser(BaseParser):
     def parse(self, blob):
         relationships = []
         for match in self.line(blob):
-            found = [f.value for f in self.source.facts
-                     if f.trait == 'host.process.unauthorized' and f.value in match.lower()]
-            for mp in self.mappers:
-                source = self.set_value(mp.source, found, self.used_facts)
-                target = self.set_value(mp.target, found, self.used_facts)
-                relationships.append(
-                    Relationship(source=(mp.source, source),
-                                 edge=mp.edge,
-                                 target=(mp.target, target))
-                )
+            for found in [f.value for f in self.source.facts if f.trait == 'host.process.unauthorized' and f.value in match.lower()]:
+                for mp in self.mappers:
+                    source = self.set_value(mp.source, found, self.used_facts)
+                    target = self.set_value(mp.target, found, self.used_facts)
+                    relationships.append(
+                        Relationship(source=(mp.source, source),
+                                     edge=mp.edge,
+                                     target=(mp.target, target))
+                    )
         return relationships
