db1afebd005ce5ce4720878c57852ade7eedab3f
Amy Janett <ajanett@mitre.org>
2020-03-17 16:07:51 -0400

    Add UserId property to the Select-Object call

--- payloads/Get-Record-IDs.ps1
+++ payloads/Get-Record-IDs.ps1
@@ -3,5 +3,5 @@
 $events = @();
 $lognames = 'Microsoft-Windows-Powershell/Operational', 'Security', 'Windows PowerShell';
 foreach ($logname in $lognames) {
-    $events += $(Get-WinEvent -FilterHashTable @{LogName = $logname; Data = $process_id} -ErrorAction SilentlyContinue | Select-Object -Property RecordId, ProviderName);
+    $events += $(Get-WinEvent -FilterHashTable @{LogName = $logname; Data = $process_id} -ErrorAction SilentlyContinue | Select-Object -Property RecordId, ProviderName, UserId);
 }
