a89e0bc2968c3038b321b6e531412330b390bb91
garunagiri <garunagiri@mitre.org>
2020-03-20 09:58:35 -0400

    some more changes

--- app/response_planner.py
+++ app/response_planner.py
@@ -5,8 +5,15 @@ class LogicalPlanner:
         self.planning_svc = planning_svc
         self.stopping_conditions = stopping_conditions
         self.stopping_condition_met = False
+        self.handled_detections = []
+        self.severity = 0
 
     async def execute(self, phase):
         for link in await self.planning_svc.get_links(operation=self.operation, phase=phase,
                                                       stopping_conditions=self.stopping_conditions, planner=self):
+            if link.ability.tactic == 'detection':
+                await self.operation.apply(link)
+            elif link.ability.tactic == 'hunt':
+                if any(uf not in self.handled_detections for uf in link.used):
+                    await self.operation.apply(link)
             await self.operation.apply(link)
