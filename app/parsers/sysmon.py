226eec83a4f63c1d3d406e9eec58337248857644
Melanie Chan <61020544+mchan143@users.noreply.github.com>
2020-03-31 09:12:08 -0400

    VIRTS-1043: Collect System Information (#12)
    * Filter logs for last 24hrs and get PID's child processes
    
    * Ignore ParentProcessGuids and set source to PID
    
    * Query Sysmon adversary, collect sys info from GUID
    
    * Removed outdated files
    
    * formatting fix

---
+++ app/parsers/sysmon.py
@@ -0,0 +1,40 @@
+import re
+
+from app.objects.secondclass.c_relationship import Relationship
+from app.utility.base_parser import BaseParser
+
+
+class Parser(BaseParser):
+
+    def parse(self, blob):
+        relationships = []
+        events = [event for event in blob.split('\r\n\r\n') if event != '']
+        for event in events:
+            for mp in self.mappers:
+                match = self.parse_options[mp.target.split('.').pop()](event)
+                if match:
+                    guid = [f.value for f in self.used_facts if f.trait == mp.source].pop()
+                    relationships.append(Relationship(source=(mp.source, guid),
+                                                      edge=mp.edge,
+                                                      target=(mp.target, match.group(1))))
+        return relationships
+
+    @property
+    def parse_options(self):
+        return dict(
+            eventid=self.parse_eventid,
+            recordid=self.parse_recordid,
+            user=self.parse_user
+        )
+
+    @staticmethod
+    def parse_eventid(event):
+        return re.search(r'\bId\s*: (.*)', event, re.IGNORECASE)
+
+    @staticmethod
+    def parse_recordid(event):
+        return re.search(r'RecordId\s*: (.*)', event, re.IGNORECASE)
+
+    @staticmethod
+    def parse_user(event):
+        return re.search(r'User: (.*)', event, re.IGNORECASE)
--- payloads/Get-Record-IDs.ps1
+++
@@ -1,7 +0,0 @@
-﻿param($process_id)
-
-$events = @();
-$lognames = 'Microsoft-Windows-Powershell/Operational', 'Security', 'Windows PowerShell';
-foreach ($logname in $lognames) {
-    $events += $(Get-WinEvent -FilterHashTable @{LogName = $logname; Data = $process_id} -ErrorAction SilentlyContinue | Select-Object -Property RecordId, ProviderName, UserId);
-}
