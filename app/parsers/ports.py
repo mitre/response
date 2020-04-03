042f78044486515ab01b771089b20f5194dd02db
Alex <43142871+khyberspache@users.noreply.github.com>
2020-04-03 13:39:43 -0400

    VIRTS-1076: CALDIRA Abilities (#14)
    - Identify malicious local ports ability
    - Find malicious outbound connections ability
    - Build inbound/outbound firewall rules
    - Add basic requirements enforcement and JSON parser function

---
+++ app/parsers/ports.py
@@ -0,0 +1,18 @@
+from app.objects.secondclass.c_relationship import Relationship
+from app.utility.base_parser import BaseParser
+
+
+class Parser(BaseParser):
+
+    def parse(self, blob):
+        relationships = []
+        for match in self.load_json(blob):
+            for mp in self.mappers:
+                source = self.set_value(mp.source, match['pid'], self.used_facts)
+                target = self.set_value(mp.target, match['port'], self.used_facts)
+                relationships.append(
+                    Relationship(source=(mp.source, source),
+                                 edge=mp.edge,
+                                 target=(mp.target, target))
+                )
+        return relationships
---
+++ app/requirements/base_requirement.py
@@ -0,0 +1,37 @@
+class BaseRequirement:
+
+    def __init__(self, requirement_info):
+        self.enforcements = requirement_info['enforcements']
+
+    def is_valid_relationship(self, used_facts, relationship):
+        """
+        Checks if the used facts for a link match with the list of known fact relationships
+        :param used_facts:
+        :param relationship:
+        :return: True if there is a match, False if not
+        """
+        if not self._check_edge(relationship.edge):
+            return False
+        if self.enforcements.target:
+            for fact in used_facts:
+                if self._check_target(relationship.target, fact):
+                    return True
+            return False
+        return True
+
+    """ PRIVATE """
+
+    @staticmethod
+    def _get_relationships(uf, relationships):
+        return [r for r in relationships if r.source[0] == uf.trait and r.source[1] == uf.value]
+
+    @staticmethod
+    def _check_target(target, match):
+        if target[0] == match.trait and target[1] == match.value:
+            return True
+        return False
+
+    def _check_edge(self, edge):
+        if edge == self.enforcements.edge:
+            return True
+        return False
