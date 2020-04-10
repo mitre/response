bc8d0887a0ab762722ed2bb8f1bc24e7808b7f77
Chris Lenk <clenk@mitre.org>
2020-04-10 14:29:30 -0400

    WIP changes

--- app/response_svc.py
+++ app/response_svc.py
@@ -1,26 +1,40 @@
 from aiohttp_jinja2 import template
 import asyncio
+import uuid
 
 from app.utility.base_service import BaseService
 from app.utility.event import Observer
 
 
-class LinkAddedObserver(Observer):
+class LinkCompletedObserver(Observer):
 
     def __init__(self, rest_svc):
-        Observer.__init__(self, 'link', 'added')
+        Observer.__init__(self, 'link', 'completed')
         self.rest_svc = rest_svc
 
-    def handle(self):
+    async def handle(self, agent, pid):
+        if agent.group == 'blue':
+            return
+
         access = dict(access=(self.rest_svc.Access.BLUE,))
-        data = {
-            'name': 'Auto-Collect Blue Data',
-            'group': 'blue',
-            'adversary_id': 'f61e3fc0-43d8-4b36-b5d3-710610b92974',
-            'auto_close': 1,
-        }
+
+        source_id = str(uuid.uuid4())
+        source_name = 'blue-pid-{}'.format(source_id),
+        source_data = dict(
+            id=source_id,
+            name=source_name,
+            facts=[dict(trait='host.process.id', value=pid)],
+        )
+        op_data = dict(
+            name='Auto-Collect Blue Data',
+            group='blue',
+            adversary_id='f61e3fc0-43d8-4b36-b5d3-710610b92974',
+            source=source_name,
+            auto_close=0,
+        )
         loop = asyncio.get_event_loop()
-        loop.create_task(self.rest_svc.create_operation(access, data))
+        await loop.create_task(self.rest_svc.persist_source(source_data))
+        loop.create_task(self.rest_svc.create_operation(access, op_data))
 
 
 class ResponseService(BaseService):
@@ -30,7 +44,7 @@ class ResponseService(BaseService):
         self.data_svc = services.get('data_svc')
 
         rest_svc = services.get('rest_svc')
-        LinkAddedObserver.register(rest_svc)
+        LinkCompletedObserver.register(rest_svc)
 
     @template('response.html')
     async def splash(self, request):
