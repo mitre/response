a26e56fd9e87794c8b04eb1f6ad6e3c12da3694c
Chris Lenk <clenk@mitre.org>
2020-04-10 16:46:27 -0400

    Update to use per-agent tasking

--- app/response_svc.py
+++ app/response_svc.py
@@ -1,40 +1,30 @@
 from aiohttp_jinja2 import template
-import asyncio
 import uuid
 
+from app.objects.secondclass.c_fact import Fact
+from app.objects.c_operation import Operation
+from app.objects.c_source import Source
 from app.utility.base_service import BaseService
 from app.utility.event import Observer
 
 
+BLUE_GROUP = 'blue'
+BLUE_ADVERSARY = 'f61e3fc0-43d8-4b36-b5d3-710610b92974'
+BLUE_OP_NAME = 'Auto-Collect Response Data'
+
+
 class LinkCompletedObserver(Observer):
 
-    def __init__(self, rest_svc):
+    def __init__(self, response_svc):
         Observer.__init__(self, 'link', 'completed')
-        self.rest_svc = rest_svc
+        self.response_svc = response_svc
 
     async def handle(self, agent, pid):
-        if agent.group == 'blue':
+        if agent.group == BLUE_GROUP:
             return
 
-        access = dict(access=(self.rest_svc.Access.BLUE,))
-
-        source_id = str(uuid.uuid4())
-        source_name = 'blue-pid-{}'.format(source_id),
-        source_data = dict(
-            id=source_id,
-            name=source_name,
-            facts=[dict(trait='host.process.id', value=pid)],
-        )
-        op_data = dict(
-            name='Auto-Collect Blue Data',
-            group='blue',
-            adversary_id='f61e3fc0-43d8-4b36-b5d3-710610b92974',
-            source=source_name,
-            auto_close=0,
-        )
-        loop = asyncio.get_event_loop()
-        await loop.create_task(self.rest_svc.persist_source(source_data))
-        loop.create_task(self.rest_svc.create_operation(access, op_data))
+        links, facts = await self.response_svc.respond_to_pid(pid, agent)
+        await self.response_svc.create_operation(links, facts)
 
 
 class ResponseService(BaseService):
@@ -42,12 +32,45 @@ class ResponseService(BaseService):
     def __init__(self, services):
         self.log = self.add_service('response_svc', self)
         self.data_svc = services.get('data_svc')
+        self.rest_svc = services.get('rest_svc')
+        self.agents = []
+        self.abilities = []
 
-        rest_svc = services.get('rest_svc')
-        LinkCompletedObserver.register(rest_svc)
+        LinkCompletedObserver.register(self)
 
     @template('response.html')
     async def splash(self, request):
         abilities = [a for a in await self.data_svc.locate('abilities') if await a.which_plugin() == 'response']
         adversaries = [a for a in await self.data_svc.locate('adversaries') if await a.which_plugin() == 'response']
         return dict(abilities=abilities, adversaries=adversaries)
+
+    async def respond_to_pid(self, pid, agent):
+        await self.refresh_blue_agents_abilities()
+        facts = [Fact(trait='host.process.id', value=pid)]
+        total_links = []
+
+        for blue_agent in self.agents:
+            for step in self.abilities:
+                links = await self.rest_svc.task_agent_with_ability(blue_agent, step, facts)
+                for link in links:
+                    facts.extend(link.facts)
+                total_links.extend(links)
+
+        return total_links, facts
+
+    async def refresh_blue_agents_abilities(self):
+        blue_agents = await self.data_svc.locate('agents', match=dict(group='blue'))
+        self.agents = [agent.paw for agent in blue_agents]
+
+        blue_adversary = await self.data_svc.locate('adversaries', match=dict(adversary_id=BLUE_ADVERSARY))
+        self.abilities = [a.ability_id for a in blue_adversary[0].atomic_ordering]
+
+    async def create_operation(self, links, facts):
+        access = dict(access=(self.rest_svc.Access.BLUE,))
+        source_id = str(uuid.uuid4())
+        source_name = 'blue-pid-{}'.format(source_id),
+        source = Source(identifier=source_id, name=source_name, facts=facts)
+        await self.get_service('data_svc').store(source)
+        op = Operation(name=BLUE_OP_NAME, agents=self.agents, adversary=BLUE_ADVERSARY,
+                       source=source, state='finished', access=access)
+        await self.get_service('data_svc').store(op)
