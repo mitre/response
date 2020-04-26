392845384b23da9dc4dca1f83f1936f8dddd1e19
garunagiri <garunagiri@mitre.org>
2020-04-26 17:58:10 -0400

    changes

--- app/response_planner.py
+++ app/response_planner.py
@@ -6,31 +6,28 @@ class LogicalPlanner:
         self.stopping_conditions = stopping_conditions
         self.stopping_condition_met = False
         self.processed_links = []
-        self.handled_detections = []    # stores completed links that are processed
+        self.handled_detections = []
         self.severity = 0
 
     async def execute(self, **kwargs):
         self.process_completed_links()
-        links = await self.planning_svc.get_links(operation=self.operation, phase=1,
+        links = await self.planning_svc.get_links(operation=self.operation,
                                                   stopping_conditions=self.stopping_conditions, planner=self)
         to_apply, detections_being_handled = self.select_links(links)
         for link in to_apply:
             await self.operation.apply(link)
-        self.handled_detections.extend(detections_being_handled)
+        self.handled_detections.extend(list(detections_being_handled))
 
     def select_links(self, links):
         to_apply = []
-        detections_being_handled = []
+        detections_being_handled = set()
         for link in links:
             if link.ability.tactic == 'detection':
                 to_apply.append(link)
-            elif link.ability.tactic == 'hunt':
-                if any(uf not in self.handled_detections for uf in link.used):
-                    to_apply.append(link)
-            elif link.ability.tactic == 'response':
-                if any(uf not in self.handled_detections for uf in link.used):
-                    to_apply.append(link)
-                    detections_being_handled.append(uf for uf in link.used if uf not in self.detections_to_handle)
+            elif any(uf not in self.handled_detections for uf in link.used) and link.ability.tactic in ['hunt', 'response']:
+                to_apply.append(link)
+                if link.ability.tactic == 'response':
+                    detections_being_handled.update(link.used)
         return to_apply, detections_being_handled
 
     def process_completed_links(self):
