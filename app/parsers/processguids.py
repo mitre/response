e7ecf062f66875f92de43ead1a3a8298c1975009
Engstrom, Frederick <fengstrom@mitre.org>
2020-05-14 01:18:19 -0500

    fix for relationship consistency

--- app/parsers/processguids.py
+++ app/parsers/processguids.py
@@ -1,5 +1,6 @@
 import re
 
+from app.objects.secondclass.c_fact import Fact
 from app.objects.secondclass.c_relationship import Relationship
 from app.utility.base_parser import BaseParser
 
@@ -15,8 +16,8 @@ class Parser(BaseParser):
                 source = self.set_value(mp.source, src_fact_value, self.used_facts)
                 target = self.set_value(mp.target, guid, self.used_facts)
                 relationships.append(
-                    Relationship(source=(mp.source, source),
+                    Relationship(source=Fact(mp.source, source),
                                  edge=mp.edge,
-                                 target=(mp.target, target))
+                                 target=Fact(mp.target, target))
                 )
         return relationships
