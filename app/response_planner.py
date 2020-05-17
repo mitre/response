84d51b781c3888a6e2cf08b6f27abea3ea2040e3
garunagiri <garunagiri@mitre.org>
2020-05-17 15:20:33 -0400

    working on moving over to bucket planner style

--- app/response_planner.py
+++ app/response_planner.py
@@ -5,8 +5,58 @@ class LogicalPlanner:
         self.planning_svc = planning_svc
         self.stopping_conditions = stopping_conditions
         self.stopping_condition_met = False
-        self.handled_detections = []
+        self.state_machine = ['detection', 'hunt', 'response']
+        self.next_bucket = 'detection'
+        self.links_hunted = []
+        self.links_responded = []
         self.severity = 0
+
+    async def execute(self):
+        await self.planning_svc.execute_planner(self)
+
+    async def detection(self):
+        links = self.planning_svc.get_links(planner=self, operation=self.operation, bucket=['hunt'])
+        self.next_bucket = await self.planning_svc.default_next_bucket('detection', self.state_machine)
+
+    async def hunt(self):
+        links = self.planning_svc.get_links(planner=self, operation=self.operation, bucket=['hunt'])
+        to_apply = []
+        for link in links:
+            if any(parent not in self.links_hunted for parent in self._get_parent_links(link)):
+                to_apply.append(link)
+        self.next_bucket = await self.planning_svc.default_next_bucket('hunt', self.state_machine)
+        pass
+
+    async def response(self):
+        links = self.planning_svc.get_links(planner=self, operation=self.operation, bucket=['response'])
+        self.next_bucket = 'detection'
+        pass
+
+    """ PRIVATE """
+
+    def _get_parent_links(self, link):
+        parent_links = []
+        parent_links.extend(self._links_with_fact_as_relationship(fact) for fact in link.used)
+        return parent_links
+
+    def _links_with_fact_as_relationship(self, fact):
+        links_with_fact = []
+        for link in self.operation.chain:
+            if any(self._fact_in_relationship(fact, rel) for rel in link.relationships):
+                links_with_fact.append(link)
+        return links_with_fact
+
+    @staticmethod
+    def _fact_in_relationship(fact, relationship):
+        for f in [relationship.source, relationship.target]:
+            if f.trait == fact.trait and f.value == fact.value:
+                return True
+        return False
+
+
+
+
+
 
     async def execute(self, **kwargs):
         links = await self.planning_svc.get_links(operation=self.operation,
@@ -14,7 +64,7 @@ class LogicalPlanner:
         to_apply, detections_being_handled = self.select_links(links)
         for link in to_apply:
             await self.operation.apply(link)
-        self.handled_detections.extend(list(detections_being_handled))
+        self.handled_detection_and_response_links.extend(list(detections_being_handled))
 
     def select_links(self, links):
         to_apply = []
@@ -22,7 +72,7 @@ class LogicalPlanner:
         for link in links:
             if link.ability.tactic == 'detection':
                 to_apply.append(link)
-            elif any(uf not in self.handled_detections for uf in link.used) and \
+            elif any(uf not in self.handled_detection_and_response_links for uf in link.used) and \
                     link.ability.tactic in ['hunt', 'response']:
                 to_apply.append(link)
                 if link.ability.tactic == 'response':
