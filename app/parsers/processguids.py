051187133524d9680dcff1f45deb7f2bbc54361a
Melanie Chan <61020544+mchan143@users.noreply.github.com>
2020-05-28 15:53:06 -0400

    bug fix for process guids

--- app/parsers/processguids.py
+++ app/parsers/processguids.py
@@ -12,7 +12,7 @@ class Parser(BaseParser):
         guids = re.findall(r'\bProcessGuid: {(.*)}', blob, re.IGNORECASE)
         for guid in guids:
             for mp in self.mappers:
-                src_fact_value = [f.value for f in self.source_facts if f.trait == mp.source].pop()
+                src_fact_value = [f.value for f in self.used_facts if f.trait == mp.source].pop()
                 source = self.set_value(mp.source, src_fact_value, self.used_facts)
                 target = self.set_value(mp.target, guid, self.used_facts)
                 relationships.append(
